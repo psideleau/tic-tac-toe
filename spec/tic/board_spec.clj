6e419c9111c7a6cd0968d7d876c170205b55dc52
Paul Sideleau <paul.sideleau@surveysampling.com>
2016-01-18 00:29:10 -0500

    full implementation of minimax algorithmm

--- spec/tic/board_spec.clj
+++ spec/tic/board_spec.clj
@@ -5,96 +5,96 @@
 (describe "the tic-tac-toe board"
   (context "a new game"
     (it "should create an empty board"
-      (should= [[:empty :empty :empty]
-                [:empty :empty :empty]
-                [:empty :empty :empty]] (board/new-board)))))
+      (should= [[:_ :_ :_]
+                [:_ :_ :_]
+                [:_ :_ :_]] (board/new-board)))))
 
-(declare ^:dynamic piece)
+(declare ^:dynamic square)
 
 (describe "the state of a new game"
   (around [it]
     (doseq [p [0 1 2 3 4 5 6 7 8]]
-      (binding [piece p]
+      (binding [square p]
         (it))))
 
-    (it (str "all board pieces should be free")
+    (it (str "all board squares should be free")
       (let [board (board/new-board)]
-        (should (board/free? board piece)))))
+        (should (board/free? board square)))))
 
 (describe "check if player has won the game"
   (context "player X by going accross"
     (it (str "x should win top row")
-      (let [board [[:X :X :X] [:empty :empty :empty] [:empty :empty :empty]]]
+      (let [board [[:X :X :X] [:_ :_ :_] [:_ :_ :_]]]
         (should (board/winner? board :X))))
     (it (str "x should win middle row")
-      (let [board [[:empty :empty :empty] [:X :X :X] [:empty :empty :empty]]]
+      (let [board [[:_ :_ :_] [:X :X :X] [:_ :_ :_]]]
         (should (board/winner? board :X))))
     (it (str "x should win bottom row")
-      (let [board [[:empty :empty :empty] [:empty :empty :empty] [:X :X :X]]]
+      (let [board [[:_ :_ :_] [:_ :_ :_] [:X :X :X]]]
         (should (board/winner? board :X))))
 
     (it (str "x should not win accross top row")
-      (let [board [[:X :O :X] [:empty :empty :empty] [:empty :empty :empty]]]
+      (let [board [[:X :O :X] [:_ :_ :_] [:_ :_ :_]]]
         (should-not (board/winner? board :X))))
     (it (str "x should not win accross middle row")
-      (let [board [[:empty :empty :empty] [:X :empty :X] [:empty :empty :empty]]]
+      (let [board [[:_ :_ :_] [:X :_ :X] [:_ :_ :_]]]
         (should-not (board/winner? board :X))))
     (it (str "x should not win accross bottom row")
-      (let [board [[:empty :empty :empty] [:empty :empty :empty] [:X :X :O]]]
+      (let [board [[:_ :_ :_] [:_ :_ :_] [:X :X :O]]]
         (should-not (board/winner? board :X)))))
 
   (context "player O by going accross"
     (it (str "O should win accross top row")
-      (let [board [[:O :O :O] [:empty :empty :empty] [:empty :empty :empty]]]
+      (let [board [[:O :O :O] [:_ :_ :_] [:_ :_ :_]]]
         (should (board/winner? board :O))))
     (it (str "O should win accross middle row")
-      (let [board [[:empty :empty :empty] [:O :O :O] [:empty :empty :empty]]]
+      (let [board [[:_ :_ :_] [:O :O :O] [:_ :_ :_]]]
         (should (board/winner? board :O))))
     (it (str "O should win accross bottom row")
-      (let [board [[:empty :empty :empty] [:empty :empty :empty] [:O :O :O]]]
+      (let [board [[:_ :_ :_] [:_ :_ :_] [:O :O :O]]]
         (should (board/winner? board :O))))
 
     (it (str "O should not win accross top row")
-      (let [board [[:O :O :X] [:empty :empty :empty] [:empty :empty :empty]]]
+      (let [board [[:O :O :X] [:_ :_ :_] [:_ :_ :_]]]
         (should-not (board/winner? board :O))))
     (it (str "O should not win accross middle row")
-      (let [board [[:empty :empty :empty] [:O :empty :O] [:empty :empty :empty]]]
+      (let [board [[:_ :_ :_] [:O :_ :O] [:_ :_ :_]]]
         (should-not (board/winner? board :O))))
     (it (str "O should not win accross bottom row")
-      (let [board [[:empty :empty :empty] [:empty :empty :empty] [:O :X :O]]]
+      (let [board [[:_ :_ :_] [:_ :_ :_] [:O :X :O]]]
         (should-not (board/winner? board :O)))))
 
 
   (context "player X by winning going top-to-bottom or vice-versa"
     (it (str "x should win left col")
       (let [board [[:X :O :O]
-                   [:X :empty :empty]
+                   [:X :_ :_]
                    [:X :O :O]]]
         (should (board/winner? board :X))))
     (it (str "x should win middle col")
-      (let [board [[:empty :X :empty]
-                   [:O :X :empty]
-                   [:empty :X :empty]]]
+      (let [board [[:_ :X :_]
+                   [:O :X :_]
+                   [:_ :X :_]]]
         (should (board/winner? board :X))))
     (it (str "x should win right row")
-      (let [board [[:empty :empty :X]
-                   [:empty :empty :X]
+      (let [board [[:_ :_ :X]
+                   [:_ :_ :X]
                    [:O :O :X]]]
         (should (board/winner? board :X)))))
 
   (context "player O by winning going top-to-bottom or vice-versa"
     (it (str "O should win left col")
       (let [board [[:O :X :X]
-                   [:O :empty :empty]
+                   [:O :_ :_]
                    [:O :X :X]]]
         (should (board/winner? board :O))))
     (it (str "O should win middle col")
-      (let [board [[:empty :O :empty]
-                   [:O :O :empty]
-                   [:empty :O :empty]]]
+      (let [board [[:_ :O :_]
+                   [:O :O :_]
+                   [:_ :O :_]]]
         (should (board/winner? board :O))))
     (it (str "O should win right row")
-      (let [board [[:empty :empty :O] [:empty :empty :O] [:X :O :O]]]
+      (let [board [[:_ :_ :O] [:_ :_ :O] [:X :O :O]]]
         (should (board/winner? board :O)))))
 
 
@@ -110,52 +110,71 @@
                    [:X :O :O]]]
         (should (board/winner? board :X)))))
 
-(context "should take a spot"
-  (it (str "should take top left ")
-    (let [board (board/new-board)]
-      (should= [[:X :empty :empty]
-                [:empty :empty  :empty]
-                [:empty :empty :empty]] (board/move board :X 0))))
-  (it (str "should take top middle ")
-    (let [board (board/new-board)]
-      (should= [[:empty :X :empty]
-                [:empty :empty  :empty]
-                [:empty :empty :empty]] (board/move board :X 1))))
-  (it (str "should take top right ")
-    (let [board (board/new-board)]
-      (should= [[:empty :empty :X]
-                [:empty :empty :empty]
-                [:empty :empty :empty]] (board/move board :X 2))))
-  (it (str "should take middle left ")
-    (let [board (board/new-board)]
-      (should= [[:empty :empty :empty]
-                [:X :empty  :empty]
-                [:empty :empty :empty]] (board/move board :X 3))))
-  (it (str "should take middle middle ")
-    (let [board (board/new-board)]
-      (should= [[:empty :empty :empty]
-                [:empty :X  :empty]
-                [:empty :empty :empty]] (board/move board :X 4))))
-  (it (str "should take middle right ")
-    (let [board (board/new-board)]
-      (should= [[:empty :empty :empty]
-                [:empty :empty :X]
-                [:empty :empty :empty]] (board/move board :X 5))))
-  (it (str "should take bottom left ")
-    (let [board (board/new-board)]
-      (should= [[:empty :empty :empty]
-                [:empty :empty  :empty]
-                [:X :empty :empty]] (board/move board :X 6))))
-  (it (str "should take bottom middle ")
-    (let [board (board/new-board)]
-      (should= [[:empty :empty :empty]
-                [:empty :empty  :empty]
-                [:empty :X :empty]] (board/move board :X 7))))
-  (it (str "should take bottom right ")
-    (let [board (board/new-board)]
-      (should= [[:empty :empty :empty]
-                [:empty :empty :empty]
-                [:empty :empty :X]] (board/move board :X 8))))))
+  (context "should take a spot"
+    (it (str "should take top left ")
+      (let [board (board/new-board)]
+        (should= [[:X :_ :_]
+                  [:_ :_  :_]
+                  [:_ :_ :_]] (board/take-square board :X 0))))
+    (it (str "should take top middle ")
+      (let [board (board/new-board)]
+        (should= [[:_ :X :_]
+                  [:_ :_  :_]
+                  [:_ :_ :_]] (board/take-square board :X 1))))
+    (it (str "should take top right ")
+      (let [board (board/new-board)]
+        (should= [[:_ :_ :X]
+                  [:_ :_ :_]
+                  [:_ :_ :_]] (board/take-square board :X 2))))
+    (it (str "should take middle left ")
+      (let [board (board/new-board)]
+        (should= [[:_ :_ :_]
+                  [:X :_  :_]
+                  [:_ :_ :_]] (board/take-square board :X 3))))
+    (it (str "should take middle middle ")
+      (let [board (board/new-board)]
+        (should= [[:_ :_ :_]
+                  [:_ :X  :_]
+                  [:_ :_ :_]] (board/take-square board :X 4))))
+    (it (str "should take middle right ")
+      (let [board (board/new-board)]
+        (should= [[:_ :_ :_]
+                  [:_ :_ :X]
+                  [:_ :_ :_]] (board/take-square board :X 5))))
+    (it (str "should take bottom left ")
+      (let [board (board/new-board)]
+        (should= [[:_ :_ :_]
+                  [:_ :_  :_]
+                  [:X :_ :_]] (board/take-square board :X 6))))
+    (it (str "should take bottom middle ")
+      (let [board (board/new-board)]
+        (should= [[:_ :_ :_]
+                  [:_ :_  :_]
+                  [:_ :X :_]] (board/take-square board :X 7))))
+    (it (str "should take bottom right ")
+      (let [board (board/new-board)]
+        (should= [[:_ :_ :_]
+                  [:_ :_ :_]
+                  [:_ :_ :X]] (board/take-square board :X 8)))))
+
+  (context "two perfect players will end the game in a draw"
+    (it "should be a tie"
+      (should  (board/tie? [ [:X :X :O]
+                             [:O :O :X]
+                             [:X :O :O]] :X :O)))
+    (it "should not be a tie"
+      (should-not  (board/tie? [[:X :X :_]
+                            [:O :O :X]
+                            [:X :O :O]] :X :O))))
+
+  (context "the state of a board"
+    (it "all positions should be free"
+     (should= [0 1 2 3 4 5 6 7 8] (board/open-squares (board/new-board))))
+
+    (it "open poistion should be free"
+      (should= [1 3 8] (board/open-squares [[:X :_ :O]
+                                            [:_ :O :X]
+                                            [:O :X :_]])))))
 
 
 
--- src/tic/board.clj
+++ src/tic/board.clj
@@ -1,66 +1,75 @@
 (ns tic.board)
 
 (defn- empty-row []
-  (vec (take 3 (repeat :empty))))
+  (vec (take 3 (repeat :_))))
 
-(defn- coordinate [piece]
-  {:row (quot piece 3) :col (rem piece 3)})
+(defn- coordinate [square]
+  {:row (quot square 3) :col (rem square 3)})
 
-(defn- get-board-piece-val [board row col]
+(defn- board-square-val [board row col]
   (nth (nth board row) col))
 
-(defn- get-board-piece [board piece]
-  (let [coordinate  (coordinate piece)]
-    (get-board-piece-val board (:row coordinate) (:col coordinate))))
+(defn- get-board-square [board square]
+  (let [coordinate  (coordinate square)]
+    (board-square-val board (:row coordinate) (:col coordinate))))
 
 (defn- play-marked-all-of? [col player]
-  (every? (fn [piece] (= piece player)) col))
+  (every? (fn [square] (= square player)) col))
 
 (defn- is-winner-accross? [board player]
   (some #(play-marked-all-of? % player) board))
 
-(defn- get-piece-vals-top-to-bottom [board col]
+(defn- square-vals-top-to-bottom [board col]
   (for [row (range 3)]
-    (get-board-piece-val board row col)))
+    (board-square-val board row col)))
 
-(defn- is-winner-top-to-bottom? [board player]
+(defn- winner-top-to-bottom? [board player]
   (or
-    (play-marked-all-of? (get-piece-vals-top-to-bottom board 0) player)
-    (play-marked-all-of? (get-piece-vals-top-to-bottom board 1) player)
-    (play-marked-all-of? (get-piece-vals-top-to-bottom board 2) player)))
+    (play-marked-all-of? (square-vals-top-to-bottom board 0) player)
+    (play-marked-all-of? (square-vals-top-to-bottom board 1) player)
+    (play-marked-all-of? (square-vals-top-to-bottom board 2) player)))
 
-(defn- get-piece-vals-top-left-to-bottom-right [board]
+(defn- square-vals-top-left-to-bottom-right [board]
   (for [idx (range 3) ]
-    (get-board-piece-val board idx idx)))
+    (board-square-val board idx idx)))
 
-(defn- get-piece-vals-bottom-left-to-top-right [board]
+(defn- square-vals-bottom-left-to-top-right [board]
   (loop [row 2 col 0 vals []]
     (if (> col 2)
       vals
-      (recur (dec row) (inc col) (conj  vals (get-board-piece-val board row col))))))
+      (recur (dec row) (inc col) (conj  vals (board-square-val board row col))))))
 
-(defn- is-winner-diagonal? [board player]
+(defn- winner-diagonal? [board player]
   (or
-    (play-marked-all-of? (get-piece-vals-top-left-to-bottom-right board) player)
-    (play-marked-all-of? (get-piece-vals-bottom-left-to-top-right board) player)))
+    (play-marked-all-of? (square-vals-top-left-to-bottom-right board) player)
+    (play-marked-all-of? (square-vals-bottom-left-to-top-right board) player)))
 
 (defn winner? [board player]
   (or
     (is-winner-accross? board player)
-    (is-winner-top-to-bottom? board player)
-    (is-winner-diagonal? board player)
+    (winner-top-to-bottom? board player)
+    (winner-diagonal? board player)
   ))
 
-(defn free? [board piece]
-  (= :empty (get-board-piece board piece)))
+(defn free? [board square]
+  (= :_ (get-board-square board square)))
 
 (defn new-board[]
   (vec (take 3 (repeatedly empty-row))))
 
-(defn mark-col [col player idx]
+(defn- mark-col [col player idx]
   (assoc col idx player))
 
-(defn move [board player piece]
-  (let [coordinate (coordinate piece)]
+(defn take-square [board player square]
+  (let [coordinate (coordinate square)]
     (assoc board (:row coordinate) (mark-col (nth board (:row coordinate)) player (:col coordinate)))
     ))
+
+(defn- game-over? [board]
+  (every? #(not (free? board %)) (range 9)))
+
+(defn tie? [board player1 player2]
+  (and (game-over? board) (not (or (winner? board player1) (winner? board player2)))))
+
+(defn open-squares [board]
+  (filter #(free? board %) (range 9)))
