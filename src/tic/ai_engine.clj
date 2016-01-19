e5f0ba6108132eb8bf6b55c18c4a4406f0095a0e
Paul Sideleau <paul.sideleau@surveysampling.com>
2016-01-18 23:35:14 -0500

    code cleanup

--- src/tic/ai_engine.clj
+++ src/tic/ai_engine.clj
@@ -1,34 +1,38 @@
 (ns tic.ai-engine
   (:require [tic.board :as board]))
 
-(declare minimax)
+(declare minimax-square)
 (declare minimax-for-open-postions)
-
+(def ^:const next-state-needed-for-score -1)
 (defn- next-player [first-player second-player player-turn]
   (if (= first-player player-turn)
     second-player
     first-player))
+
+
+(defn score [board current-player opposing-player]
+  (cond
+    (board/winner? board current-player) 10
+    (board/winner? board opposing-player) -10
+    (board/tie?    board current-player opposing-player) 0
+    :else next-state-needed-for-score))
 
 (defn- minimax-square [board
                       current-player
                       opposing-player
                       player-turn
                       square]
-  (let [new-board (board/take-square board player-turn square)]
-    (cond
-      (board/winner? new-board current-player) 10
-      (board/winner? new-board opposing-player) -10
-      (board/tie?    new-board current-player opposing-player) 0
-      :else (let [next-player (next-player current-player opposing-player player-turn)
-                  next-board-states (board/open-squares new-board)]
-              (minimax-for-open-postions new-board next-board-states current-player opposing-player next-player)))))
+  (let [new-board (board/take-square board player-turn square)
+        score (score new-board current-player opposing-player) ]
+    (if (not= score next-state-needed-for-score)
+      score
+      (let [next-player (next-player current-player opposing-player player-turn)
+            next-board-states (board/open-squares new-board)
+            scores (map #(trampoline minimax-square new-board current-player opposing-player next-player %) next-board-states)]
+        (if (= next-player current-player)
+          (apply max scores)
+          (apply min scores))))))
 
-(defn- minimax-for-open-postions [board open-squares current-player opposing-player player-turn]
-   (let [scores (map #(minimax-square board current-player opposing-player player-turn %) open-squares)]
-     (if (= player-turn current-player)
-       (apply max scores)
-       (apply min scores)
-       )))
 
 (defn minimax
   ([board current-player opposing-player]
